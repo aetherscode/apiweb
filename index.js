4b3f1dd736ed147a94aaf912fc389a0193063673
aetherscode <aetherscode@gmail.com>
2024-07-09 07:28:57 +0700

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
-const { inject } = require("@vercel/analytics");
 const helloRoute = require("./routes/helloRoute");
 const tiktokRoute = require("./routes/tiktokRoute");
 const igstalkRoute = require("./routes/igStalk");
@@ -14,51 +13,42 @@ const allDownloader = require("./routes/allDownloader");
 const ronaldo = require("./routes/random/ronaldo");
 const messi = require("./routes/random/messi");
 const apalah = require("./routes/apalah");
+const mlstalk = require("./routes/mlstalk");
+const ttstalkRoute = require("./routes/ttStalk");
 
 const swaggerAssetsRoute = require("./routes/swaggerAssetsRoute");
-module.exports = require('./index.js');
-
-
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-// Middleware untuk mem-parsing JSON bodies
 app.use(express.json());
 
-// CSS kustom untuk mengubah tampilan Swagger UI
-
-// Serve Swagger documentation beserta CSS kustom
 app.use("/api-docs", swaggerAssetsRoute);
 app.get("/swagger.json", (req, res) => {
   const swaggerPath = path.join(__dirname, "/swagger.json");
-
   const swaggerJson = fs.readFileSync(swaggerPath, "utf-8");
-
   res.setHeader("Content-Type", "application/json");
-
   res.send(swaggerJson);
 });
-// Routes
+
 app.use("/", helloRoute);
 app.use("/", tiktokRoute);
 app.use("/", igstalkRoute);
 app.use("/", aiRoute);
 app.use("/", igdlRoute);
-app.use ("/", ghStalk);
-app.use ("/", allDownloader);
-app.use ("/", ffstalk);
-app.use ("/", cuaca);
-app.use ("/", ronaldo);
-app.use ("/", messi);
-app.use ("/", apalah);
+app.use("/", ghStalk);
+app.use("/", allDownloader);
+app.use("/", ffstalk);
+app.use("/", cuaca);
+app.use("/", ronaldo);
+app.use("/", messi);
+app.use("/", apalah);
+app.use("/", mlstalk);
+app.use("/", ttstalkRoute);
 
-
-// Route untuk halaman utama
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server sedang berjalan, mendengarkan port ${PORT}`);
 });
